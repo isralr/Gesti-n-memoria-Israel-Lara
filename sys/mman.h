f92932c62626f8e639d94e9f6184d2866cd2fbe9
isralr <ilaramac@myuax.com>
2024-11-14 12:27:10 +0100

    linuz windows

--- sys/mman.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by israe on 14/11/2024.
-//
-
-#ifndef MMAN_H
-#define MMAN_H
-
-#endif //MMAN_H
--- sys/wait.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by israe on 14/11/2024.
-//
-
-#ifndef WAIT_H
-#define WAIT_H
-
-#endif //WAIT_H
