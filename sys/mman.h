eba9eaf0655f17e8b19143f2d9f46ac6a15c102f
isralr <ilaramac@myuax.com>
2024-11-14 12:10:54 +0100

    creado directorio sys junto .h

---
+++ sys/mman.h
@@ -0,0 +1,8 @@
+//
+// Created by israe on 14/11/2024.
+//
+
+#ifndef MMAN_H
+#define MMAN_H
+
+#endif //MMAN_H
---
+++ sys/wait.h
@@ -0,0 +1,8 @@
+//
+// Created by israe on 14/11/2024.
+//
+
+#ifndef WAIT_H
+#define WAIT_H
+
+#endif //WAIT_H
